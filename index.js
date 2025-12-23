5e2ab3be5d3bf15fb8a09aea46f53cda10031df3
vehxi <vehxi.dev@gmail.com>
2025-12-23 23:45:52 +0300

    Final: add output formatting

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Входные данные: температура в градусах Цельсия
 const celsius = 25
+
 console.log("Температура в Цельсиях:", celsius)
+
 // Формула пересчета
 const fahrenheit = (celsius * 9/5) + 32
+
+console.log(`Результат: ${celsius}°C равно ${fahrenheit}°F`)
