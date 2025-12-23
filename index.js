2d1f73cdac6bc81a2ca2a4dcb2bf4638bd490948
vehxi <vehxi.dev@gmail.com>
2025-12-23 23:42:57 +0300

    Add: temperature conversation logic

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 // Входные данные: температура в градусах Цельсия
-const celsius = 25;
-console.log("Температура в Цельсиях:", celsius);
+const celsius = 25
+console.log("Температура в Цельсиях:", celsius)
+// Формула пересчета
+const fahrenheit = (celsius * 9/5) + 32
