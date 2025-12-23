899bf23a9d63ba1275013ca4cc8a19ddd5f4e46f
vehxi <vehxi.dev@gmail.com>
2025-12-23 23:51:44 +0300

    Add: tmp conversation

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ console.log("Температура в Цельсиях:", celsius)
 
 // Формула пересчета
 const fahrenheit = (celsius * 9/5) + 32
+const kelvin = celsius + 273.15
 
 console.log(`Результат: ${celsius}°C равно ${fahrenheit}°F`)
